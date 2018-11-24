3df59f83a8584fb4711b32018162425955c13820
Anibal Ferracini <anibal.ferracini.comercial@gmail.com>
2018-11-24 13:57:18 -0200

    Atualização de Código

--- src/main/java/Trello.java
+++ src/main/java/Trello.java
@@ -1,6 +1,43 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.E;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
+
 public class Trello {
-    public Trello() {
-        Dado("^que esteja logado no trello$", () -> {
-        });
+    @Dado("^que esteja logado no trello$")
+    public void queEstejaLogadoNoTrello() {
+        System.out.println("queEstejaLogadoNoTrello");
+    }
+
+    @E("^acesse o board$")
+    public void acesseOBoard() {
+        System.out.println("acesseOBoard");
+    }
+
+    @Quando("^crio um card com o nome \"([^\"]*)\"$")
+    public void crioUmCardComONome(String arg0) throws Throwable {
+        System.out.println("crioUmCardComONome");
+    }
+
+    @E("^comento \"([^\"]*)\"$")
+    public void comento(String arg0) throws Throwable {
+        System.out.println("comento");
+    }
+
+    @Entao("^o card deve ester na lista$")
+    public void oCardDeveEsterNaLista() {
+        System.out.println("oCardDeveEsterNaLista");
+    }
+
+    @Quando("^excluo o card$")
+    public void excluoOCard() {
+        System.out.println("excluoOCard");
+        
+    }
+
+    @Entao("^o card nao existe mais$")
+    public void oCardNaoExisteMais() {
+        System.out.println("oCardNaoExisteMais");
     }
 }
